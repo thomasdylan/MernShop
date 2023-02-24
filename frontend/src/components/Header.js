08f6a4e95cb8ab3d3e4fc7b6b2a11df248b7cbe2
Dylan Thomas <thomasdylan76@gmail.com>
2023-02-23 18:14:32 -0700

    Added dynamic product description page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,10 +14,10 @@ const Header = () => {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
                             <LinkContainer to='/cart'>
-                                <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                                <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
                             </LinkContainer>
                             <LinkContainer to='/login'>
-                                <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
+                                <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>
                             </LinkContainer>
 
                         </Nav>
