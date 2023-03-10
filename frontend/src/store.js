61311b84fda7ba2fb5c96a6119d86510108773f6
Dylan Thomas <thomasdylan76@gmail.com>
2023-03-09 18:22:27 -0700

    Added product detail reducer and action for redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
-import { productListReducer } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 
 const store = configureStore({
-    reducer: { productList: productListReducer },
+    reducer: {
+        productList: productListReducer,
+        productDetails: productDetailsReducer
+    },
     preloadedState: {},
     middleware: [thunk],
 })
