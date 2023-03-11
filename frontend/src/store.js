0a62cb5a9b83cb7fca5552a85744dca8966f688a
Dylan Thomas <thomasdylan76@gmail.com>
2023-03-10 21:14:14 -0700

    Created cart reducers and actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const store = configureStore({
     reducer: {
         productList: productListReducer,
-        productDetails: productDetailsReducer
+        productDetails: productDetailsReducer,
+        cart: cartReducer
     },
-    preloadedState: {},
+    preloadedState: {
+        cart: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+    },
     middleware: [thunk],
 })
 
