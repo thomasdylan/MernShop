d15a665ca97295107b0504408756bc3566137a03
Dylan Thomas <thomasdylan76@gmail.com>
2023-04-03 20:22:46 -0600

    Cart functionality, controllers and route cleanup

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
-import { AddToCart } from '../actions/cartActions'
+import { AddToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
     const productId = match.params.id
@@ -22,7 +22,11 @@ const CartScreen = ({ match, location, history }) => {
     }, [dispatch, productId, qty]);
 
     const removeFromCartHandler = (id) => {
-        console.log('remove')
+        dispatch(removeFromCart(id))
+    }
+
+    const checkoutHandler = () => {
+        history.push('/login?redirect=shipping')
     }
 
     return (
@@ -71,7 +75,16 @@ const CartScreen = ({ match, location, history }) => {
                 <Card>
                     <ListGroup variant='flush'>
                         <ListGroup.Item>
-                            <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
+                            <h2>
+                                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                                items
+                            </h2>
+                            ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler}>
+                                Proceed to checkout
+                            </Button>
                         </ListGroup.Item>
                     </ListGroup>
                 </Card>
